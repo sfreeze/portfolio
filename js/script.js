d6a3529a0a79a537a03d838096444b391bdd9fdf
sfreeze <soeonh1115@gmail.com>
2021-09-08 12:34:18 +0900

    사이드내비 동작 수정 및 반응형 위치 변경

--- js/script.js
+++ js/script.js
@@ -1,5 +1,45 @@
-// 팝업 열기
+
 $(document).ready(function() {
+    // 스크롤 맞춰 내비 이동
+    var $menu     = $('.side_nav a'),
+        $list     = $('.side_nav ul'),
+        $contents = $('.page'),
+        $doc      = $('html, body');
+
+    $(function() {
+        // 해당 섹션으로 스크롤 이동
+        $menu.on('click', 'a', function(e){
+            var $target = $(this).parent(),
+                idx     = $target.index(),
+                section = $contents.eq(idx),
+                offsetTop = section.offset().top;
+            $doc.stop()
+                    .animate({
+                        scrollTop :offsetTop
+                    }, 800);
+            return false;
+        });
+    });
+
+    // menu class 추가
+    $(window).scroll(function(){
+        var scltop = $(window).scrollTop();
+        $.each($contents, function(idx, item){
+            var $target   = $contents.eq(idx),
+                i         = $target.index(),
+                targetTop = $target.offset().top;
+
+            if (targetTop <= scltop) {
+                $menu.removeClass('on');
+                $menu.eq(idx).addClass('on');
+            }
+            if (!(0 <= scltop)) {
+                $menu.removeClass('on');
+            }
+        });
+    });
+
+    // 팝업 열기
     $('.btn-open').on('click', function() {
         $('.popup').show();
         return false;
@@ -15,3 +55,4 @@ $(document).mouseup(function (e){
         LayerPopup.hide();
     }
 });
+
